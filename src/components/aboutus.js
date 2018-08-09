daa733f33fbbbce7be89118ad4c9d3293d0826c9
Diana-Costru <diana.costru@gmail.com>
2018-08-09 22:41:34 +0300

    changes in aboutus and projects

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -17,13 +17,14 @@ class About extends Component {
                  
             </div>
 
-            <h2 style={{paddingTop: '0.5em', textAlign: 'center', fontFamily:'Vollkorn', fontWeight:'bold', marginRight:'-150px'}}>Costru Diana</h2>
+            <h2 style={{ textAlign: 'center', fontFamily:'Vollkorn', fontWeight:'bold', marginRight:'-150px'}}>Costru Diana</h2>
             <h4 style={{color: '#e60000', textAlign: 'center', fontWeight:'bold', fontFamily:'Dancing Script', fontSize:'35px', marginRight:'-150px'}}>Founder of Teaching Portal</h4>
           </Cell>
 
+          <h1 style={{paddingTop:'0.5em', textAlign: 'center', fontFamily:'Vollkorn', fontWeight:'bold', marginRight:'-150px', marginLeft:'50px'}}>What is Teaching Portal?</h1>
           <Cell col={4}>
           <div>
-          <p style={{textAlign: 'left', fontFamily:'Lora', fontSize:'20px', marginRight:'-440px', marginLeft:'10px', padding:'4.5em', textIndent:'20px', lineHeight:'1.7'}}>
+          <p style={{textAlign: 'left', fontFamily:'Lora', fontSize:'23px', marginRight:'4px', marginLeft:'-580px', padding:'5.5em', textIndent:'20px', lineHeight:'1.7'}}>
           Teaching portal is a community designed to prepare students for their exams at any subjects. Here all the applicants have 
           the opportunity to be taught by the best teachers through videos or articles. Also they put a lot of tasks which are similar
            with the exam items, in that way the student will be sure that after some course he will be able to pass the exam with succes.
@@ -32,6 +33,66 @@ class About extends Component {
           </p>
           </div>
           </Cell>
+
+         {/* ------------------------------- */}
+
+          <Cell col={4}>
+
+<div style={{textAlign: 'center'}}>
+  <img
+    src="curly.png"
+    alt="avatar"
+    style={{height: '350px', padding:'1em', marginRight:'-2150px'}}
+     />
+     
+</div>
+
+<h2 style={{ textAlign: 'center', fontFamily:'Vollkorn', fontWeight:'bold', marginRight:'-2150px'}}>Lucy Miller</h2>
+<h4 style={{color: '#e60000', textAlign: 'center', fontWeight:'bold', fontFamily:'Dancing Script', fontSize:'35px', marginRight:'-2150px'}}>Manager of Teaching Portal</h4>
+</Cell>
+
+<h1 style={{paddingTop:'0.5em', textAlign: 'center', fontFamily:'Vollkorn', fontWeight:'bold', marginRight:'150px', marginLeft:'-380px'}}>Why you should choose us?</h1>
+<Cell col={4}>
+<div>
+<p style={{textAlign: 'left', fontFamily:'Lora', fontSize:'23px', marginRight:'300px', marginLeft:'-940px', padding:'5.5em', textIndent:'20px', lineHeight:'1.7'}}>
+Teaching Portal is a global community, welcoming people from every discipline and culture who seek 
+a deeper understanding of the world. We believe passionately in the power of ideas to change attitudes,
+ lives and, ultimately, the world. On Teaching Portal.com, we're building a clearinghouse of free knowledge 
+ from the world's most inspired thinkers — and a community of curious souls to engage with ideas and each 
+ other, both online and at Teaching Portal and TEDx events around the world, all year long.
+In fact, everything we do — from our Conferences to our  Teaching Portal Talks to the projects sparked by 
+the  Teaching Portal Prize, from the global Teaching Portal community to the Teaching Portal lesson series
+ — is driven by this goal: How can we best spread great ideas?
+
+</p>
+</div>
+</Cell>
+
+{/* ------------------------------- */}
+<Cell col={4}>
+
+<div style={{textAlign: 'center'}}>
+  <img
+    src="sam.png"
+    alt="avatar"
+    style={{height: '350px', marginRight:'-150px'}}
+     />
+     
+</div>
+
+<h2 style={{ textAlign: 'center', fontFamily:'Vollkorn', fontWeight:'bold', marginRight:'-150px'}}>Nancy Sullivan</h2>
+<h4 style={{color: '#e60000', textAlign: 'center', fontWeight:'bold', fontFamily:'Dancing Script', fontSize:'35px', marginRight:'-150px'}}>Student at the Teaching Portal</h4>
+</Cell>
+
+<h1 style={{ textAlign: 'center', fontFamily:'Vollkorn', fontWeight:'bold', marginRight:'-150px', marginLeft:'50px'}}>Opinion of our users</h1>
+<Cell col={4}>
+<div>
+<p style={{textAlign: 'left', fontFamily:'Lora', fontSize:'23px', marginRight:'4px', marginLeft:'-450px', padding:'4.5em', textIndent:'20px', lineHeight:'1.7'}}>
+Very explained even for a short course. It's not for full understanding, but it works perfectly 
+as an introduction for the subject. And you can really build a robot after taking this course.
+</p>
+</div>
+</Cell>
         </Grid>
       </div>
     )
--- src/components/projects.js
+++ src/components/projects.js
@@ -16,7 +16,7 @@ class Projects extends Component {
           {/* Project 1 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#000', height: '176px', background: 'url(book.jpg) center / cover'}} >#1 The power of fiction</CardTitle>
-            <CardText>
+            <CardText style={{fontFamily:'Volkorn'}}>
             Listening to stories widens the imagination; telling them lets us leap over cultural walls, 
             embrace different experiences, feel what others feel. Elif Shafak builds on this simple idea
              to argue that fiction can overcome identity politics.
@@ -33,7 +33,7 @@ class Projects extends Component {
           {/* Project 2 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#000', height: '176px', background: 'url(door.jpg) center / cover'}} >#2 Why a good book is a secret door</CardTitle>
-            <CardText>
+            <CardText style={{fontFamily:'Volkorn'}}>
             Childhood is surreal. Why shouldn't children's books be? In this whimsical talk, award-winning author 
             Mac Barnett speaks about writing that escapes the page, art as a doorway to wonder --
            and what real kids say to a fictional whale.
@@ -50,7 +50,7 @@ class Projects extends Component {
           {/* Project 3 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#000', height: '176px', background: 'url(mind.jpg) center / cover'}} >#3 How books can open our mind</CardTitle>
-            <CardText>
+            <CardText style={{fontFamily:'Volkorn'}}>
             What happens when a dream you've held since childhood ... doesn't come true? 
             As Lisa Bu adjusted to a new life in the United States, she turned to books to expand her
              mind and create a new path for herself.
@@ -67,7 +67,7 @@ class Projects extends Component {
           {/* Project 4 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
             <CardTitle style={{color: '#000', height: '176px', background: 'url(girl.jpg) center / cover'}} >#4 Finding the story inside the painting</CardTitle>
-            <CardText>
+            <CardText style={{fontFamily:'Volkorn'}}>
             When Tracy Chevalier looks at paintings, she imagines the stories behind them: How did the painter meet
              his model? What would explain that look in her eye? Why is that man ... blushing? 
             </CardText>
@@ -91,7 +91,7 @@ class Projects extends Component {
         {/* Project 1 */}
         <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(algoritmjpg.jpg) center / cover'}} >#1 The influence of algorithms</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           Algorithms play a big part in our day-to-day lives. From search engines to architecture, 
           explore how these formulas affect...
           </CardText>
@@ -107,7 +107,7 @@ class Projects extends Component {
          {/* Project 2 */}
          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(math.jpg) center / cover'}} >#2 Math talks to blow your mind</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           Numbers, patterns and equations are at the core of these talks, which will teach 
           you how to fold better origami and how to quantify history.
           </CardText>
@@ -123,7 +123,7 @@ class Projects extends Component {
          {/* Project 3 */}
          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(love.jpg) center / cover'}} >#3 Math in unexpected places</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           These talks are here to set the record straight and illuminate the unexpected ways formulas and fractions
            influence everything...
           </CardText>
@@ -139,7 +139,7 @@ class Projects extends Component {
          {/* Project 4 */}
          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(second.jpg) center / cover'}} >#4 A love poem for lonely prime numbers</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           Performance poet (and math student) Harry Baker spins a love poem about his favorite kind of numbers
            -- the lonely, love-lorn prime. 
           </CardText>
@@ -161,12 +161,12 @@ class Projects extends Component {
         {/* Project 1 */}
         <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(dna.jpg) center / cover'}} >#1 How does DNA works?</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           Your DNA makes you, you -- but how does it work? These talks explore what we know about
            the genome, the unique genetic sequence that makes up life as we know it.
           </CardText>
           <CardActions border>
-            <Button colored>Ted Talk</Button>
+            <Button colored >Ted Talk</Button>
             <Button colored>Read Article</Button>
           </CardActions>
           <CardMenu style={{color: '#fff'}}>
@@ -177,7 +177,7 @@ class Projects extends Component {
          {/* Project 2 */}
          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(brain.jpg) center / cover'}} >#2 The complexity of memory</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           The mind is a diligent recorder, taking note of all that happens and storing data on it for retrieval later, 
           right? Well actually, no. Enjoy these 7 illuminating talks on the science...
           </CardText>
@@ -193,7 +193,7 @@ class Projects extends Component {
          {/* Project 3 */}
          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(leaf.jpg) center / cover'}} >#3 Beautiful tricks of flowers</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           In this visually dazzling talk, Jonathan Drori shows the extraordinary ways flowering plants -- 
           over a quarter million species -- have evolved to attract insects to spread their pollen...
           </CardText>
@@ -209,7 +209,7 @@ class Projects extends Component {
          {/* Project 4 */}
          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(bio.jpg) center / cover'}} >#4 Reconnect with nature</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           Be inspired to go out and rediscover the wonders of the natural world with these talks
            that will give you a deeper understanding of where the wild things grow.
           </CardText>
@@ -231,7 +231,7 @@ class Projects extends Component {
         {/* Project 1 */}
         <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(einstein.jpg) center / cover'}} >#1 The fascinating physics of evertday life</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           Physics doesn't just happen in a fancy lab -- it happens when you push a piece of 
           buttered toast off the table or drop a couple of raisins in a fizzy drink or watch a
            coffee spill dry.
@@ -248,7 +248,7 @@ class Projects extends Component {
          {/* Project 2 */}
          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(sphere.jpg) center / cover'}} >#2 Making sense of a visible quantum object</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           Physicists are used to the idea that subatomic particles behave according to the bizarre rules of quantum mechanics, 
           completely different to human-scale objects. 
           </CardText>
@@ -264,7 +264,7 @@ class Projects extends Component {
          {/* Project 3 */}
          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(circuit.jpg) center / cover'}} >#3 An 8-dimensional model of the universe</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           Physicist and surfer Garrett Lisi presents a controversial new model of the universe
            that -- just maybe -- answers all the big questions.
           </CardText>
@@ -280,11 +280,11 @@ class Projects extends Component {
          {/* Project 4 */}
          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
           <CardTitle style={{color: '#000', height: '176px', background: 'url(physics.jpg) center / cover'}} >#4 Just how small is an atom</CardTitle>
-          <CardText>
+          <CardText style={{fontFamily:'Volkorn'}}>
           Just how small are atoms? Really, really, really small. This fast-paced animation from TED-Ed uses metaphors
           </CardText>
           <CardActions border>
-            <Button colored>Ted Talk</Button>
+            <Button colored >Ted Talk</Button>
             <Button colored>Read Article</Button>
           </CardActions>
           <CardMenu style={{color: '#fff'}}>
@@ -301,10 +301,10 @@ class Projects extends Component {
     return(
       <div>
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-          <Tab>Literature</Tab>
-          <Tab>Math</Tab>
-          <Tab>Biology</Tab>
-          <Tab>Physics</Tab>
+          <Tab style={{fontFamily:'Abril Fatface'}}>Literature</Tab>
+          <Tab style={{fontFamily:'Abril Fatface'}}>Math</Tab>
+          <Tab style={{fontFamily:'Abril Fatface'}}>Biology</Tab>
+          <Tab style={{fontFamily:'Abril Fatface'}}>Physics</Tab>
         </Tabs>
 
 
